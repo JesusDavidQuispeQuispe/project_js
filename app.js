094c24cc0c55e5ffc015fcc397fee61947a69bb4
jesus <jumbo7u7@gmail.com>
2025-01-10 19:20:16 -0500

    add style in project_jv01

---
+++ app.js
@@ -0,0 +1,30 @@
+// selecionar elemntos de htlm
+
+
+const list_name = doccument.queryselector('#nombres');
+const btn_obtener = document.querySelector("#btn-obtener");
+const result = document.querySelector('#resultado');
+const modal = document.querySelector('.modal');
+const spinner = document.querySelector('.spinner');
+
+//Funciones que se ejecuta al presioanr el boton 
+
+function obtener(){
+    const textareavalue = list_name.value;
+    modal.style.display = "block";
+
+    setTimeout(() => {
+        
+        
+    }, 5000);
+
+}
+
+
+
+
+
+
+
+
+let nom   
